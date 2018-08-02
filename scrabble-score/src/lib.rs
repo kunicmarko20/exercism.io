9133c98a41ce9b2df61c984f12bc97ad3bcc40cb
Marko Kunic <kunicmarko20@gmail.com>
2018-08-02 09:37:34 +0200

    scrabble score

---
+++ scrabble-score/src/lib.rs
@@ -0,0 +1,19 @@
+pub fn score(word: &str) -> u64 {
+    let mut result = 0;
+
+    for character in word.to_lowercase().chars() {
+        result += match character {
+            'a'|'e'|'i'|'o'|'u'|'l'|'n'|'r'|'s'|'t' => 1,
+            'd'|'g' => 2,
+            'b'|'c'|'m'|'p' => 3,
+            'f'|'h'|'v'|'w'|'y' => 4,
+            'k' => 5,
+            'j'|'x' => 8,
+            'q'|'z' => 10,
+            _ => 0,
+        }
+
+    }
+
+    result
+}
